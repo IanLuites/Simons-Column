cee7861ade8f5e790298ddda66ba6380a29894f8
Ian Luites <ian@luites.com>
2024-11-18 10:09:55 -0400

    ✨ [rust/tpic6c596] add a TPIC6C596 emulator

--- rust/crates/tpic6c596/src/lib.rs
+++ rust/crates/tpic6c596/src/lib.rs
@@ -1,1 +1,17 @@
 //! Control TPIC6C596 power logic 8-bit shift register.
+
+mod emulator;
+pub use emulator::{Emulator, Register};
+
+/// Represents the pins of the TPIC6C596 shift register.
+#[derive(Debug, Clone, Copy)]
+pub enum Pin {
+    /// Clock pin.
+    Clock,
+    /// Control pin.
+    Control,
+    /// Data pin.
+    Data,
+    /// Latch pin.
+    Latch,
+}
