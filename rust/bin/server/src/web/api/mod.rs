e1fff242bfaf456a5a335050e21c5fe29f545e75
Ian Luites <ian@luites.com>
2024-11-19 13:38:00 -0400

    ✨ [rust/server] add basic status endpoint

--- rust/bin/server/src/web/api/mod.rs
+++ rust/bin/server/src/web/api/mod.rs
@@ -4,7 +4,9 @@ use axum::Router;
 
 use crate::config::Config;
 
+mod status;
+
 /// API router
 pub fn router(_config: &Config) -> Router {
-    Router::new()
+    Router::new().route("/status", axum::routing::get(status::status))
 }
