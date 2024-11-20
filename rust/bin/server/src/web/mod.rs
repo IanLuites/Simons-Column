de5c58b4888231c4db346ebac664c6e02e9f0d5a
Ian Luites <ian@luites.com>
2024-11-20 10:11:11 -0400

    ✨ [rust/server] embed and host frontend ui from server

--- rust/bin/server/src/web/mod.rs
+++ rust/bin/server/src/web/mod.rs
@@ -7,9 +7,15 @@ use crate::config::Config;
 mod api;
 mod state;
 
+/// Frontend static assets.
+#[derive(rust_embed::RustEmbed, Clone)]
+#[folder = "../../../node/ui/build"]
+struct Frontend;
+
 /// Server Web requests
 pub async fn serve(config: &Config) -> std::io::Result<()> {
     let app = axum::Router::new()
+        .nest_service("/", axum_embed::ServeEmbed::<Frontend>::new())
         .nest("/api", api::router(config))
         .with_state(state::WebState::new(config));
 
